3934e3e9fc297bbd02d37b138f7702dab2df0bb2
m4rkuss <markbu07@gmail.com>
2022-10-08 14:35:10 +0300

    full task

---
+++ src/first_task/info_obj.js
@@ -0,0 +1,11 @@
+let userInfo = {
+  photo: './image.jpeg',
+  FIO: 'Бучацкий Марк Дмитриевич',
+  phone: '+380956230328',
+  email: 'markbu07@gmail.com',
+  city: 'Kyiv',
+  experience: '1 year',
+  skills: 'HTML, CSS, JS, JQuery, Bootstrap, Git, React, Figma, Webpack'
+}
+
+export default userInfo;
